5bbd13b25ce7647844bf4271c8d96da0cd7f34df
Han <allen0346@gmail.com>
2020-02-11 17:16:54 +0800

    Adjust: exclude react, react-dom, styled-components in bundle

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -24,30 +24,9 @@ module.exports = {
       },
     ],
   },
-  resolve: {
-    alias: {
-      react: path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    },
-  },
   externals: {
-    'styled-components': {
-      commonjs: 'styled-components',
-      commonjs2: 'styled-components',
-      amd: 'StyledComponents',
-      root: 'StyledComponents',
-    },
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'React',
-      root: 'React',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'ReactDOM',
-      root: 'ReactDOM',
-    },
+    react: 'react',
+    'react-dom': 'react-dom',
+    'styled-components': 'styled-components',
   },
 };
