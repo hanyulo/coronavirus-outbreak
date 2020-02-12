cb9a7795f4775e37d3dd9cbf35ee0c721dfde50c
Han <allen0346@gmail.com>
2020-02-12 10:37:17 +0800

    Adjust: add header

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import ChinaChronological from './components/ChinaChronological';
 import testData from './data/testData.json';
 
 
+const Header = styled.h1`
+  margin-bottom: 20px;
+`;
 
 const App = () => (
   <div>
+    <Header>Map Demo</Header>
     <ChinaChronological
       data={testData}
     />
