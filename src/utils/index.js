1e3a3e46e085aa15352792586f9c1af60cfa5a17
Han <allen0346@gmail.com>
2020-02-14 17:56:26 +0800

    Adjust: update throttle in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,7 +5,6 @@ function formatNumber(num) {
 function throttle(fn, interval) {
   let lastExecutedTime = null;
   let dormnatInterval = null;
-  let timer = null;
   return function decorator(...args) {
     if (lastExecutedTime) {
       dormnatInterval = Date.now() - lastExecutedTime;
@@ -13,15 +12,6 @@ function throttle(fn, interval) {
     if (!lastExecutedTime || (lastExecutedTime && (dormnatInterval >= interval))) {
       fn.apply(this, args);
       lastExecutedTime = Date.now();
-      if (timer) {
-        clearTimeout(timer);
-        timer = null;
-      }
-    } else if (lastExecutedTime && (dormnatInterval < interval)) {
-      if (!timer) {
-        console.log('timer!')
-        timer = setTimeout(() => fn.apply(this, args), interval);
-      }
     }
   };
 }
